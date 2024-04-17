cf1ff0a7be370c5fca5ed6ede845ef5751de5209
happy jiwon <sjwon113@naver.com>
2024-04-17 16:47:07 +0900

    [DESIGN] 새 페이지 추가 - home_page, list_page, setting_page

---
+++ lib/pages/list_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ListPage extends StatelessWidget {
+  const ListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('list'),),
+    );
+  }
+}
